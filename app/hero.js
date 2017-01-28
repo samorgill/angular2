709bf73f3c2841ac8dcf6ed3a4818a1b99904c85
Sam <orgillsams@gmail.com>
2017-01-28 13:32:23 +0000

    Added resuable components

---
+++ app/hero.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ssorg on 28/01/2017.
+ */
+"use strict";
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
---
+++ app/hero.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by ssorg on 28/01/2017.
+ */
+
+export class Hero {
+  id: number;
+  name: string;
+}
