033b4612d8f2c3acddda1f16d1917c8831d42f9a
Sam <orgillsams@gmail.com>
2017-01-31 21:08:19 +0000

    Added clickable hero details

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -10,17 +10,9 @@ import { HeroService } from './hero.service';
 import 'rxjs/add/operator/switchMap';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>
-`
+  templateUrl: './hero-detail.component.html',
 })
 
 export class HeroDetailComponent implements OnInit{
@@ -36,6 +28,10 @@ export class HeroDetailComponent implements OnInit{
     this.route.params
       .switchMap((params: Params) => this.heroService.getHero(+params['id']))
       .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
