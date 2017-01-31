72723eb750c768b8b406403a5efa0a15a853636b
sam.orgill <sam.orgill@ultra-as.com>
2017-01-31 17:00:34 +0000

    added routing up to find our way bacl

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,16 +4,42 @@ import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import {HeroDetailComponent} from "./hero-detail.component";
+import {HeroesComponent} from "./heroes.component";
+import {HeroService} from "./hero.service";
+import {RouterModule} from "@angular/router";
+import {DashboardComponent} from "./dashboard.component";
 
 @NgModule({
   imports:      [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
+  providers: [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -2,8 +2,12 @@
  * Created by ssorg on 28/01/2017.
  */
 
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from './hero.service';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'my-hero-detail',
@@ -19,9 +23,21 @@ import { Hero } from './hero';
 `
 })
 
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit{
   @Input()
   hero: Hero;
+
+  constructor(private heroService: HeroService,
+  private route: ActivatedRoute,
+  private location: Location
+  ){ }
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
 }
 
 
